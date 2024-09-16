7e0a90953b09cb4386a92cb37e6f036a94bb692d
DM Welch <dmw9400@gmail.com.com>
2024-09-16 18:27:22 -0400

    blorp

--- recplay.c
+++ recplay.c
@@ -20,3 +20,9 @@ typedef struct {
     SNDFILE *file;
     SF_INFO sfinfo;
 } AudioData;
+
+
+static int recordCallback(const void *input, void *output, unsigned long frameCount, const PaStreamCallbackTimeInfo *timeInfo,
+PaStreamCallbackFlags statusFlags, void *userData) {
+// Your code here
+}
