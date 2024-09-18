2901e5490cc53b9e89554410d9b32d872954626d
DM Welch <dmw9400@gmail.com.com>
2024-09-18 10:35:05 -0400

    blorp

--- recplay.c
+++ recplay.c
@@ -22,7 +22,26 @@ typedef struct {
 } AudioData;
 
 
-static int recordCallback(const void *input, void *output, unsigned long frameCount, const PaStreamCallbackTimeInfo *timeInfo,
-PaStreamCallbackFlags statusFlags, void *userData) {
-// Your code here
+// static int recordCallback(const void *input, void *output, unsigned long frameCount,    const PaStreamCallbackTimeInfo *timeInfo,
+// PaStreamCallbackFlags statusFlags, void *userData) {
+//     // count = sf_writef_float (sndfile, obuf, num_output_frames);
+
+// }
+
+
+static int recordCallback(const void *input, void *output,
+                          unsigned long frameCount,
+                          const PaStreamCallbackTimeInfo *timeInfo,
+                          PaStreamCallbackFlags statusFlags,
+                          void *userData) {
+    AudioData *audioData = (AudioData*)userData;
+    const short *in = (const short*)input;
+
+    // Write the input audio data to the file
+    if (sf_writef_short(audioData->file, in, frameCount) != frameCount) {
+        printf("Error writing to file\n");
+        return paComplete;  // End the stream on error
+    }
+
+    return paContinue;  // Continue processing audio
 }
