46f780775c32f7aff0c5e5d93932f27efddf3f65
DM Welch <dmw9400@gmail.com.com>
2024-09-20 12:39:32 -0400

    nightmare nightmare

--- recplay.c
+++ recplay.c
@@ -60,29 +60,115 @@ void print_device_info(const PaDeviceInfo *deviceInfo) {
     printf("Max Output Channels: %d\n", deviceInfo->maxOutputChannels);
 }
 
-int main(int argc, char *argv[]){
-    PaError err;
-    PaStream *stream;
-    AudioData audioData;
-    memset(&audioData.sfinfo, 0, sizeof(SF_INFO));
-    audioData.sfinfo.samplerate = SAMPLE_RATE;
-    audioData.sfinfo.channels = CHANNELS;
-    audioData.sfinfo.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
-    if (!audioData.file) {
+// int main(int argc, char *argv[]){
+//     PaError err;
+//     PaStream *stream;
+//     AudioData audioData;
+//     memset(&audioData.sfinfo, 0, sizeof(SF_INFO));
+//     audioData.sfinfo.samplerate = SAMPLE_RATE;
+//     audioData.sfinfo.channels = CHANNELS;
+//     audioData.sfinfo.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
+//     if (!audioData.file) {
+//             printf("Error opening file for recording: %s\n", sf_strerror(NULL));
+//             return 1;
+//         } else {
+//         // Playback mode: open the file for reading
+//         audioData.file = sf_open(argv[2], SFM_READ, &audioData.sfinfo);
+//         if (!audioData.file) {
+//             printf("Error opening file for playback: %s\n", sf_strerror(NULL));
+//             return 1;
+//         }
+//     }
+    
+//     // initscr();
+//     // noecho();
+//     // getch();
+//     // endwin();
+//     return 0;
+// }
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("Usage: %s <r|p> <filename.wav>\n", argv[0]);
+        return 1;
+    }
+
+    // Initialize PortAudio
+    PaError err = Pa_Initialize();
+    if (err != paNoError) {
+        printf("PortAudio error: %s\n", Pa_GetErrorText(err));
+        return 1;
+    }
+
+    AudioData data;
+    memset(&data, 0, sizeof(AudioData));
+    
+    // Parse command-line arguments
+    int isRecording = (argv[1][0] == 'r');
+    if (isRecording) {
+        // Recording mode: open the file for writing
+        data.sfinfo.samplerate = SAMPLE_RATE;
+        data.sfinfo.channels = CHANNELS;
+        data.sfinfo.format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
+        data.file = sf_open(argv[2], SFM_WRITE, &data.sfinfo);
+        if (!data.file) {
             printf("Error opening file for recording: %s\n", sf_strerror(NULL));
             return 1;
-        } else {
+        }
+    } else {
         // Playback mode: open the file for reading
-        audioData.file = sf_open(argv[2], SFM_READ, &audioData.sfinfo);
-        if (!audioData.file) {
+        data.file = sf_open(argv[2], SFM_READ, &data.sfinfo);
+        if (!data.file) {
             printf("Error opening file for playback: %s\n", sf_strerror(NULL));
             return 1;
         }
     }
-    
-    // initscr();
-    // noecho();
-    // getch();
-    // endwin();
+
+    // Print device info
+    const PaDeviceInfo *deviceInfo = Pa_GetDeviceInfo(Pa_GetDefaultInputDevice());
+    initscr();  // Start ncurses
+    print_device_info(deviceInfo);
+
+    // Open the PortAudio stream
+    PaStream *stream;
+    if (isRecording) {
+        err = Pa_OpenDefaultStream(&stream, CHANNELS, 0, paInt16, SAMPLE_RATE,
+                                   FRAMES_PER_BUFFER, recordCallback, &data);
+    } else {
+        err = Pa_OpenDefaultStream(&stream, 0, CHANNELS, paInt16, data.sfinfo.samplerate,
+                                   FRAMES_PER_BUFFER, playCallBack, &data);
+    }
+
+    if (err != paNoError) {
+        printw("Error opening PortAudio stream: %s\n", Pa_GetErrorText(err));
+        endwin();
+        Pa_Terminate();
+        return 1;
+    }
+
+    // Start the stream
+    err = Pa_StartStream(stream);
+    if (err != paNoError) {
+        printw("Error starting PortAudio stream: %s\n", Pa_GetErrorText(err));
+        endwin();
+        Pa_Terminate();
+        return 1;
+    }
+
+    // Handle user input and stream termination
+    timeout(100);  // Set input timeout for ncurses
+    int ch;
+    while ((ch = getch()) != 'q' && ch != 'Q') {
+        if (Pa_IsStreamActive(stream) == 0)
+            break;
+    }
+
+    // Clean up resources
+    Pa_StopStream(stream);
+    Pa_CloseStream(stream);
+    sf_close(data.file);
+    Pa_Terminate();
+    endwin();  // End ncurses
+
     return 0;
 }
