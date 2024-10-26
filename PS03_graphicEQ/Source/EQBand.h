ef46cad2de161b3c696fc105c8fd2774f868d3b3
DM Welch <dmw9400@gmail.com.com>
2024-10-26 19:55:04 -0400

    eqBand header set up

--- PS03_graphicEQ/Source/EQBand.h
+++ PS03_graphicEQ/Source/EQBand.h
@@ -9,3 +9,26 @@
 */
 
 #pragma once
+#include <JuceHeader.h>
+
+class EQBand : public juce::Component, public juce::Slider::Listener {
+public:
+    EQBand();
+    void resized() override;
+    
+    void initializeVTS(juce::AudioProcessorValueTreeState& vts);
+    void prepare(float frequency, int sampleRate, float gain);
+    void process(juce::dsp::AudioBlock<float>& block);
+    void reset();
+private:
+    std::vector<juce::IIRFilter> filter;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> gainAttachment;
+    int numChannels, sampleRate;
+    float frequency, gain;
+    juce::Slider bandSlider;
+    juce::Label bandLabel;
+};
+
+
+
+
